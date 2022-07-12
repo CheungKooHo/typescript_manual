f6b1a85b841b573cd70e664762f0af75127c829e
CheungKooHo <577873656@qq.com>
2022-07-12 11:53:46 +0800

    ScorePanel

--- snake/src/index.ts
+++ snake/src/index.ts
@@ -2,7 +2,7 @@
  * @Author: Coan
  * @Date: 2022-07-11 16:21:54
  * @LastEditors: Coan
- * @LastEditTime: 2022-07-12 11:14:44
+ * @LastEditTime: 2022-07-12 11:32:54
  * @FilePath: /typescript_manual/snake/src/index.ts
  * @Description:
  */
@@ -26,6 +26,29 @@ class Food {
       (Math.round(Math.random() * 29) * 10).toString() + 'px';
   }
 }
-const food = new Food();
-food.change();
-console.log(food.X, food.Y);
+
+class ScorePanel {
+  score: number = 0;
+  level: number = 1;
+  scoreEle: HTMLElement = document.getElementById('score')!;
+  levelEle: HTMLElement = document.getElementById('level')!;
+  maxLevel: number;
+  constructor(maxLevel: number = 7) {
+    this.maxLevel = maxLevel;
+  }
+  addScore() {
+    this.scoreEle.innerHTML = ++this.score + '';
+    if (!(this.score % 10)) {
+      this.levelUp();
+    }
+  }
+  levelUp() {
+    if (this.level < this.maxLevel) {
+      this.levelEle.innerHTML = ++this.level + '';
+    }
+  }
+}
+const scorePanel = new ScorePanel();
+// setInterval(() => {
+//   scorePanel.addScore();
+// }, 100);
